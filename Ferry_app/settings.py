313dbb03adabc22220b775f1274324c890edf68b
dhouhabelakhel <dhouhabelakhel2001@gmail.com>
2025-06-30 20:01:50 +0200

    corriger requ

--- Ferry_app/settings.py
+++ Ferry_app/settings.py
@@ -59,11 +59,11 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
 
 ]
